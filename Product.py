a3187f269b0b49c90bca592a17a7c3631274f164
M_Sai_Bharadwaj <114256791+bharadwaz868@users.noreply.github.com>
2022-09-28 21:55:28 +0530

    Add files via upload

---
+++ Product.py
@@ -0,0 +1,20 @@
+# Catogery-1
+
+Item_Name = ['Milk', 'Cheese', 'Butter', 'Curd', 'Ghee', 'Paneer']
+Item_Number = [101, 102, 103, 104, 105, 106]
+Price = [25, 120, 150, 20, 135, 75]
+Stock = [120, 30, 45, 100, 20, 75]
+
+# Catogery2 Fruits
+
+Item_Name2 = ['Apple','Mango','Banana','Orange','Grapes','Pine Apple','Guva','Strawberry','Watermelon']
+Item_Number2 = [201,202,203,204,205,206,207,208,209]
+Price2 = [80, 70,50,45,40,75,35,100,50]
+Stock2 = [120,130,145,100,80,85,45,75,50]
+
+# Catogery 3 Vegetables
+
+Item_Name3 = ['Tomato','Carrot','Beetroot','Califlower','Potato','Brinjal','Ladiesfinger','Leafy Vegetables','Mirchi','Cabbage','Onions']
+Item_Number3 = [301,302,303,304,305,306,307,308,309,310,311]
+Price3 = [40, 35,30,15,30,25,20,10,20,22,30]
+Stock3 = [80,50,45,60,70,75,45,25,50,20,125]
